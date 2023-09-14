1c9b6051f6b4f559aff1f0413ec60a223a535d5f
rbezugly <roman.bezugly@luxoft.com>
2023-09-14 12:20:49 +0300

    add config.py with db settings from environments variables refactor database.py to use config variables

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+DB_DBNAME = os.getenv("DB_DBNAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://hhjlhyfq:ONT49WzR6XsAbdWqcHnWx5lCJXvb4Ou4@snuffleupagus.db.elephantsql.com/hhjlhyfq"
+from config import *
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DBNAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
